62f431e2e65879a7e00a60917b591cc50f4df94e
Bhavya0921 <boombro42@gmail.com>
2026-06-25 20:56:48 +0530

    Add files via upload

--- box_oper_ovr_class.cpp
+++ box_oper_ovr_class.cpp
@@ -1,47 +1,63 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Box
-{
+class Box {
     private:
     int l;
     int b;
     int h;
     public:
-    Box()
-    {
-        l = 0;
-        b = 0;
-        h = 0;
-    }
-    Box(int l1,int b1,int h1)
+    Box(int l1,int b1,int h1)  
     {
         l = l1;
         b = b1;
         h = h1;
     }
-    Box operator<(Box b3)
+    Box() 
     {
-        Box t;
-        if(l > b3.l && b > b3.b && h > b3.h)
-            cout<<"B1 > B2\n";
-        else
-            cout<<"B1 < B2";
-        return t;
+        l = b = h = 0;
     }
-    // void display()
-    // {
-    //     cout<<l<<endl<<b<<endl<<h<<endl;
-    // }
+    Box(const Box& other) 
+    {
+       b = other.b;
+       l = other.l;
+       h = other.h;
+    }
+    int getLength()
+        { return l; }
+    int getBreadth()
+        { return b; }
+    int getHeight()
+        { return h; }
+    long long CalculateVolume() const {
+        return (long long)l * b * h;
+    }
+    bool operator<(const Box& other) const {
+        if (l < other.l) {
+            return true;
+        } else if (l == other.l) {
+            if (b < other.b) {
+                return true;
+            } else if (b == other.b) {
+                return h < other.h;
+            }
+        }
+        return false;
+    }
+    friend std::ostream& operator<<(std::ostream& out, const Box& B) {
+        out << B.l << " " << B.b << " " << B.h;
+        return out;
+    }
 };
-
-int main()
-{
-    Box b1(3 , 4, 6);
-    Box b2(2 , 3, 4);
-    Box b3;
-    b3 = b1 < b2;
-    // cout<<"B1 : \n";b1.display();
-    // cout<<"B2 : \n";b2.display();
+int main() {
+    Box b1(3, 4, 6);
+    Box b2(2, 3, 4);
+    cout << "Box 1: " << b1 << endl;
+    cout << "Box 2: " << b2 << endl;
+    if (b1 < b2) {
+        cout << "Box 1 is smaller than Box 2" << endl;
+    } else {
+        cout << "Box 1 is not smaller than Box 2" << endl;
+    }
     return 0;
 }
