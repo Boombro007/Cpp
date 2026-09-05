9a421c00ce561b12ca67c6a33ef5081a31d5be05
Bhavya0921 <boombro42@gmail.com>
2026-09-05 15:36:07 +0530

    Add files via upload

--- CirLinkedlist.cpp
+++ CirLinkedlist.cpp
@@ -6,62 +6,136 @@ class cnode
     public:
     int data;
     cnode *next;
-    cnode(int d,cnode *f)
+    cnode(int d)
     {
         data = d;
-        next = f;
+        next = NULL;
     }
 };
 
 class CLL
 {
     cnode *first;
+    cnode *tail;
     public:
     CLL()
     {
         first = NULL;
+        tail = NULL;
     }
-    int Insert(int d)
+    int IAH(int d) //insert at head
     {
         if(first == NULL)
         {
-            first = new cnode(d,first);
-            first->next = first;
+            first = tail = new cnode(d);
+            tail->next = first;
             return 1;
         }
         else
         {
-            cnode *temp = first;
-            while(temp->next != first)
-            {
-                temp = temp->next;
-            }
-            cnode *nn = new cnode(d,first);
-            temp->next = nn;
+            cnode *nn = new cnode(d);
             nn->next = first;
+            first = nn;
+            tail->next = first;
             return 1;
         }
     }
-    int Display()
+    int IAT(int d)
     {
-        cnode *temp = first;
-        while(temp->next != first)
+        if(first == NULL)
+        {
+            first = tail = new cnode(d);
+            tail->next = first;
+            return 1;
+        }
+        else
+        {
+            cnode *nn = new cnode(d);
+            nn->next = first;
+            tail->next = nn;
+            tail = nn;
+            return 1;
+        }
+    }
+    int DAH()
+    {
+        if(first == NULL) return 0;
+        else if(first == tail)
+        {
+            delete first;
+            first = tail = NULL;
+            return 1;
+        }
+        else 
+        {
+            cnode *deln = first;
+            first = first->next;
+            tail->next = first;
+            delete deln;
+            return 1;
+        }
+    }
+    int DAT()
+    {
+        if(first == NULL) return 0;
+        else if(first == tail)
+        {
+            delete first;
+            first = tail = NULL;
+            return 1;
+        }
+        else
+        {
+            cnode *temp = tail;
+            cnode *prev = first;
+            while(prev->next!= tail)
+            {
+                prev = prev->next;
+            }
+            tail = prev;
+            tail->next = first;
+            temp->next = NULL;
+            delete temp;
+            return 1;
+        }
+    }
+    void Display()
+    {
+        if(first == NULL)
+        {
+            cout<<"EMPTY!\n";
+            return;
+        }
+        cout<<first->data<<"->";
+        cnode *temp = first->next;
+        while(temp != first)
         {
             cout<<temp->data<<"->";
             temp = temp->next;
         }
-        cout<<"NULL\n";
-        return 1;
+        cout<<temp->data<<"\n";
     }
 };
 
 int main()
 {
     CLL cl;
-    cl.Insert(1);
-    cl.Insert(4);
-    cl.Insert(3);
-    cl.Insert(2);
-    cl.Display();
+    cl.IAH(1);
+    cl.IAH(4);
+    cl.IAH(3);
+    cl.IAH(2);
+    cout<<"c1 : ";
+    cl.Display(); // 2 3 4 1 2
+    cl.DAH();
+    cl.Display(); // 3 4 1 3
+
+    CLL c2;
+    c2.IAT(1);
+    c2.IAT(2);
+    c2.IAT(3);
+    cout<<"c2 : "; 
+    c2.Display(); // 1 2 3 1
+    c2.DAT();
+    c2.Display(); // 1 2 1
     return 0;
 }
